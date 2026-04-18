3b277650b2aa1f1f13d643f15b74a3862d803499
gregoriogennari-blip <gregorio.gennari@fermi.mo.it>
2026-04-18 11:39:59 +0200

    aggiornamento root file statici per render

--- vineguard/settings.py
+++ vineguard/settings.py
@@ -59,7 +59,8 @@ TIME_ZONE = "Europe/Rome"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # ===== Variabili esterne =====
