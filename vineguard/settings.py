8ab653c4cfb7d89a14441a48bb510add93b7a94d
diff-st <tommaso.gianaroli@fermi.mo.it>
2026-04-18 11:17:41 +0200

    aggiornato settings.py con variabili d'ambiente

--- vineguard/settings.py
+++ vineguard/settings.py
@@ -1,17 +1,12 @@
-INFLUX_TOKEN = "s6ZDX_S1cCzDRoeVW4-k4eizpPRLX8CYpb3tUVUZY12F80YoEEAtA76I1VBJzkw9CcfAUgqN1OAvuFiI8KnNeQ=="
-INFLUX_HOST = "https://eu-central-1-1.aws.cloud2.influxdata.com"
-INFLUX_ORG = "VINEGUARD SSH"
-INFLUX_BUCKET = "nodo_vineguard_1"
-GRAFANA_TOKEN = "tSeq4bQ1bYoWrEd3XiBAqT6eSJ-yzFysiSBBQmQJ086NV2cdsYSvHoYEuInRQg1s0hGujWlAuFvrug4RSKNa-g=="
-TELEGRAM_TOKEN ="8796899155:AAHoSo2T5Fb8knEPOl66T3i1NbJ3OGhbgew"
 from pathlib import Path
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = "dev-key"
-DEBUG = True
-ALLOWED_HOSTS = []
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "dev-key")
+DEBUG = os.getenv("DJANGO_DEBUG", "True") == "True"
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -66,3 +61,14 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# ===== Variabili esterne =====
+INFLUX_TOKEN = os.getenv("INFLUX_TOKEN", "")
+INFLUX_HOST = os.getenv("INFLUX_HOST", "https://eu-central-1-1.aws.cloud2.influxdata.com")
+INFLUX_ORG = os.getenv("INFLUX_ORG", "VINEGUARD SSH")
+INFLUX_BUCKET = os.getenv("INFLUX_BUCKET", "nodo_vineguard_1")
+
+GRAFANA_TOKEN = os.getenv("GRAFANA_TOKEN", "")
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "")
